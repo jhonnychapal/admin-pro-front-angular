edcfdbe3dd013d2640d675beba15f1cfcddd7896
Jhonny Chapal <jhonnychapal@gmail.com>
2020-07-20 16:38:41 -0500

    Diseño de la app

---
+++ src/app/shared/bradcrumbs/bradcrumbs.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bradcrumbs',
+  templateUrl: './bradcrumbs.component.html',
+  styles: [
+  ]
+})
+export class BradcrumbsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
