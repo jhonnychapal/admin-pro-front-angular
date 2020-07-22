208b63c5ebb39defe71af5841faa2e50a56e42b2
Jhonny Chapal <jhonnychapal@gmail.com>
2020-07-22 17:43:23 -0500

    Fin sección 7

---
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../../services/settings.service';
+
+@Component({
+  selector: 'app-account-settings',
+  templateUrl: './account-settings.component.html',
+  styles: [
+  ]
+})
+export class AccountSettingsComponent implements OnInit {
+
+  constructor(
+    private settingsService: SettingsService
+  ) { }
+
+  ngOnInit(): void {
+    this.settingsService.checkCurrentTheme();
+  }
+
+  changeTheme( theme: string): void {
+    this.settingsService.changeTheme( theme );
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { BradcrumbsComponent } from './bradcrumbs/bradcrumbs.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -18,7 +19,8 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
